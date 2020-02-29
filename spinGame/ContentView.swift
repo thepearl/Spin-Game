10536e4b4a49b2ad5bc63eae0534a1adc8612c38
Ghazi <ghazitozri@fsm.u-monastir.tn>
2020-02-29 11:20:53 +0100

    Initial Commit

---
+++ spinGame/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  spinGame
+//
+//  Created by Mohamed Ali BELHADJ on 2/29/20.
+//  Copyright © 2020 wimobi. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
